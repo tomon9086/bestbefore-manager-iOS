cc2d0d4ec953b7c4e4d3548c45e8877095e3a316
tomon9086 <tomo.mouse.9086@gmail.com>
2019-09-14 16:35:45 +0900

    Initial Commit

---
+++ bestbefore/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  bestbefore
+//
+//  Created by tomo on 2019/09/14.
+//  Copyright © 2019 tomo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
